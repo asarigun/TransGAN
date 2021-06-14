8ccf73399b32334513cf18165da2708f72a384c4
Ahmet Sarigun <47702994+asarigun@users.noreply.github.com>
2021-06-14 17:48:17 +0300

    cleaning unnecessary functs

--- cifar/utils.py
+++ cifar/utils.py
@@ -14,31 +14,6 @@ import numpy as np
 def noise(n_samples, z_dim, device):
         return torch.randn(n_samples,z_dim).to(device)
 
-
-#def train_loader(batch_size, img_size):
-
-#    transform = transforms.Compose([transforms.Resize(size=(img_size, img_size)).to(device),transforms.RandomHorizontalFlip().to(device),transforms.T
-#oTensor().to(device),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)).to(device)])
-
-#    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-
-#    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
-
-#    return train_loader 
-
-#def valid_loader(batch_size, img_size):
-
-#    transform = transforms.Compose([transforms.Resize(size=(img_size, img_size)).to(device),transforms.RandomHorizontalFlip().to(device),transforms.T
-#oTensor().to(device),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)).to(device)])
-
-#    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-
-#    test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
-#    valid_loader = test_loader
-
-#    return valid_loader
-
-
 def inits_weight(m):
         if type(m) == nn.Linear:
                 nn.init.xavier_uniform(m.weight.data, 1.)
